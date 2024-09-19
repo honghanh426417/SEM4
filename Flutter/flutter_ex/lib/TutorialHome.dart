8f9b045456c36791281a893ca333e1250e3d3dc3
honghanh426417 <114908283+honghanh426417@users.noreply.github.com>
2024-09-19 10:47:42 +0700

    add ex1,2

---
+++ Flutter/flutter_ex/lib/TutorialHome.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
+class TutorialHome extends StatelessWidget{
+  const TutorialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const[
+          IconButton(
+              onPressed: null,
+              icon: Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Helloo, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
